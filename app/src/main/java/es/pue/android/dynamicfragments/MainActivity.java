9870633891aaf36dad19234e40690a5497bf7324
azarrias <adolfo.zarrias@gmail.com>
2018-10-25 21:55:22 +0200

    Add button to remove fragment programmatically

--- app/src/main/java/es/pue/android/dynamicfragments/MainActivity.java
+++ app/src/main/java/es/pue/android/dynamicfragments/MainActivity.java
@@ -1,13 +1,32 @@
 package es.pue.android.dynamicfragments;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    FragmentManager fm = getSupportFragmentManager();
+    Fragment fragment = fm.findFragmentById(R.id.main_container);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (fragment == null){
+            fragment = new BlankFragment();
+            fm.beginTransaction()
+                    .add(R.id.main_container, fragment)
+                    .commit();
+        }
+    }
+
+    public void removeFragment(View view){
+        fm.beginTransaction()
+                .remove(fragment)
+                .commit();
     }
 }
